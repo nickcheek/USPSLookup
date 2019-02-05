4b9dd475b5616b7485f2fefd54955b88ca1cc72b
Nick <nick@nicholascheek.com>
2019-02-05 12:12:56 -0600

    fixed issue with Rate API

--- src/USPSLookup.php
+++ src/USPSLookup.php
@@ -108,7 +108,7 @@ class USPSLookup
 		$pack->addChild('Container','VARIABLE');
 		$pack->addChild('Size','Regular');
 		
-		$url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML='.$rate->asXML();
+		$url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=RateV4&XML='.$rate->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
