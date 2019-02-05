f7312f82e562519ac167e6c18ffc68932a90f24f
Nick <nick@nicholascheek.com>
2019-02-05 09:44:26 -0600

    Added Laravel Test

--- tests/vanillatest.php
+++ tests/vanillatest.php
@@ -2,9 +2,12 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$lookup = new \Nickcheek\USPSLookup\USPSLookup();
+$lookup = new \Nickcheek\USPSLookup\USPSLookup('XXXXXXXXX');
 $find = $lookup->Verify('3017 Province','','Benton','AR','72019');
 	    
+
 echo "<pre>";
 var_dump($find); 
 echo "</pre>";
+
+
