7067a3fb89f2349986e42581005e9cdad3fd52b0
Nick <nick@nicholascheek.com>
2019-02-05 10:59:44 -0600

    added tracking, zipcodelookup, and addresslookup

--- src/USPSLookup.php
+++ src/USPSLookup.php
@@ -30,11 +30,52 @@ class USPSLookup
 		$s = $add->addChild('State',$state);
 		$z1  = $add->addChild('Zip5',$zip);
 		$z2  = $add->addChild('Zip4');
-		
 		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=Verify&XML='.$Address->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
-			
 	}
+	
+	 public static function CityState($zip)
+    {
+	    $CityState = new \SimpleXMLElement("<CityStateLookupRequest></CityStateLookupRequest>");
+		$CityState->addAttribute('USERID', self::$user);
+		$ZipCode = $CityState->addChild('ZipCode');
+		$ZipCode->addAttribute('ID', '0');
+		$Zip  = $ZipCode->addChild('Zip5',$zip);
+		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup&XML='.$CityState->asXML();
+		$response = simplexml_load_file($url);
+			
+		return $response;		
+	}
+	
+	public static function ZipCode($address,$address2,$city,$state)
+	{
+		$Address = new \SimpleXMLElement("<ZipCodeLookupRequest></ZipCodeLookupRequest>");
+		$Address->addAttribute('USERID', self::$user);
+		$add = $Address->addChild('Address');
+		$add->addAttribute('ID', '0');
+		$a1 = $add->addChild('Address1',$address);
+		$a2 = $add->addChild('Address2',$address2);
+		$c = $add->addChild('City',$city);
+		$s = $add->addChild('State',$state);
+		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=ZipCodeLookup&XML='.$Address->asXML();
+		$response = simplexml_load_file($url);
+			
+		return $response;		
+	}
+	
+		public static function Track($trackingnumber)
+	{
+		$track = new \SimpleXMLElement("<TrackRequest></TrackRequest>");
+		$track->addAttribute('USERID', self::$user);
+		$pack = $track->addChild('TrackID');
+		$pack->addAttribute('ID', $trackingnumber);
+		$url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML='.$track->asXML();
+		$response = simplexml_load_file($url);
+			
+		return $response;		
+	}
+
+	
 }
