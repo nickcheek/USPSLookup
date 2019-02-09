70543c640d5c8ec32fadf6004875e7fee1ba610e
Nicholas Cheek <nick@nicholascheek.com>
2019-02-09 14:17:30 -0600

    removed unused variables

--- src/USPSLookup.php
+++ src/USPSLookup.php
@@ -15,39 +15,39 @@ class USPSLookup
 		}
 	}
 
-    public  function Verify($address,$address2,$city,$state,$zip)
+    public function Verify($address,$address2,$city,$state,$zip)
     {
 	    $Address = new \SimpleXMLElement("<AddressValidateRequest></AddressValidateRequest>");
 		$Address->addAttribute('USERID', $this->user);
-		$Revision = $Address->addChild('Revision','1');
+		$Address->addChild('Revision','1');
 		$add = $Address->addChild('Address');
 		$add->addAttribute('ID', '0');
-		$a1 = $add->addChild('Address1',$address);
-		$a2 = $add->addChild('Address2',$address2);
-		$c = $add->addChild('City',$city);
-		$s = $add->addChild('State',$state);
-		$z1  = $add->addChild('Zip5',$zip);
-		$z2  = $add->addChild('Zip4');
+		$add->addChild('Address1',$address);
+		$add->addChild('Address2',$address2);
+		$add->addChild('City',$city);
+		$add->addChild('State',$state);
+		$add->addChild('Zip5',$zip);
+		$add->addChild('Zip4');
 		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=Verify&XML='.$Address->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
 	}
 	
-	public  function CityState($zip)
+	public function CityState($zip)
     {
 	    $CityState = new \SimpleXMLElement("<CityStateLookupRequest></CityStateLookupRequest>");
 		$CityState->addAttribute('USERID', $this->user);
 		$ZipCode = $CityState->addChild('ZipCode');
 		$ZipCode->addAttribute('ID', '0');
-		$Zip  = $ZipCode->addChild('Zip5',$zip);
+		$ZipCode->addChild('Zip5',$zip);
 		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup&XML='.$CityState->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
 	}
 
-	public  function CityStateMultiple($zip)
+	public function CityStateMultiple($zip)
     {
 	    $CityState = new \SimpleXMLElement("<CityStateLookupRequest></CityStateLookupRequest>");
 		$CityState->addAttribute('USERID', $this->user);
@@ -55,8 +55,7 @@ class USPSLookup
 		foreach($zip as $k=>$z){
 			$ZipCode = $CityState->addChild('ZipCode');
 			$ZipCode->addAttribute('ID', $k);
-			$Zip  = $ZipCode->addChild('Zip5',$z);
-			
+			$ZipCode->addChild('Zip5',$z);
 		}
 		
 		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup&XML='.$CityState->asXML();
@@ -65,23 +64,23 @@ class USPSLookup
 		return $response;		
 	}
 	
-	public  function ZipCode($address,$address2,$city,$state)
+	public function ZipCode($address,$address2,$city,$state)
 	{
 		$Address = new \SimpleXMLElement("<ZipCodeLookupRequest></ZipCodeLookupRequest>");
 		$Address->addAttribute('USERID', $this->user);
 		$add = $Address->addChild('Address');
 		$add->addAttribute('ID', '0');
-		$a1 = $add->addChild('Address1',$address);
-		$a2 = $add->addChild('Address2',$address2);
-		$c = $add->addChild('City',$city);
-		$s = $add->addChild('State',$state);
+		$add->addChild('Address1',$address);
+		$add->addChild('Address2',$address2);
+		$add->addChild('City',$city);
+		$add->addChild('State',$state);
 		$url = 'http://production.shippingapis.com/ShippingAPITest.dll?API=ZipCodeLookup&XML='.$Address->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
 	}
 	
-	public  function Track($trackingnumber)
+	public function Track($trackingnumber)
 	{
 		$track = new \SimpleXMLElement("<TrackRequest></TrackRequest>");
 		$track->addAttribute('USERID', $this->user);
@@ -93,17 +92,15 @@ class USPSLookup
 		return $response;		
 	}
 	
-	public  function TrackMultiple($trackingarray)
+	public function TrackMultiple($trackingarray)
 	{
-		$track = new \SimpleXMLElement("<TrackRequest></TrackRequest>");
-		$track->addAttribute('USERID', $this->user);
-		
+		$mtrack = new \SimpleXMLElement("<TrackRequest></TrackRequest>");
+		$mtrack->addAttribute('USERID', $this->user);
 		foreach($trackingarray as $trackingnumber){
-			$pack = $track->addChild('TrackID');
+			$pack = $mtrack->addChild('TrackID');
 			$pack->addAttribute('ID', $trackingnumber);
 		}
-		
-		$url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML='.$track->asXML();
+		$url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML='.$mtrack->asXML();
 		$response = simplexml_load_file($url);
 			
 		return $response;		
@@ -113,7 +110,7 @@ class USPSLookup
 	{
 		$rate = new \SimpleXMLElement("<RateV4Request></RateV4Request>");
 		$rate->addAttribute('USERID', $this->user);
-		$revision = $rate->addChild("Revision",'2');
+		$rate->addChild("Revision",'2');
 		$pack = $rate->addChild('Package');
 		$pack->addAttribute('ID','0');
 		$pack->addChild('Service',$service);
